26315174bde50d934ef9c192c9378531119f761b
Onur <onurturan.t@gmail.com>
2022-04-04 06:42:58 +0300

    air and gamelab

--- scripts/crews.js
+++ scripts/crews.js
@@ -17,6 +17,7 @@ const crews = [
   new Crew(
     "Mobil Geliştirme Ekibi",
     "Flutter Framework’ü ile cross-platform uygulama geliştiren, bunun eğitimini veren ve projeler geliştirerek bilgilerini pratiğe döken AR-GE ekibidir."
+	"sources/crew/mobil.png"
   ),
   new Crew(
     "GAMELAB",
@@ -25,6 +26,7 @@ const crews = [
   new Crew(
     "AI Research",
     "Yapay zekanın çeşitli alt disiplinlerinde araştırmalar yapan; makine öğrenmesi ve derin öğrenme alanlarında çalışan AR-GE ekibidir."
+	"sources/crews/air.png"
   ),
   new Crew(
     "ALGO LAB",
